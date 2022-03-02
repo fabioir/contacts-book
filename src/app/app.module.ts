37fa5d783fb1a0b664163dc66c0a3bb66ff8e21f
Fabio <fabioiglesias@hotmail.es>
2022-03-02 17:25:56 +0100

    fix(project): remove wrong import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
-import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
 import { contactsBookReducer } from './store/reducers/contacts-book.reducer';
 
 @NgModule({
-  declarations: [AppComponent, ContactsListComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot({ contactsBook: contactsBookReducer })
