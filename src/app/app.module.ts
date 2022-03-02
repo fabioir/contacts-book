1d6daefdbf525336bbb387a8ac9c880e11a5ac04
Fabio <fabioiglesias@hotmail.es>
2022-03-02 17:47:54 +0100

    feat(toolbar): kick off toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { contactsBookReducer } from './store/reducers/contacts-book.reducer';
 
+const materialModules = [MatToolbarModule];
+
 @NgModule({
-  declarations: [AppComponent, ContactsListComponent],
+  declarations: [AppComponent, ContactsListComponent, ToolbarComponent],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ contactsBook: contactsBookReducer })
+    StoreModule.forRoot({ contactsBook: contactsBookReducer }),
+    BrowserAnimationsModule,
+    ...materialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
